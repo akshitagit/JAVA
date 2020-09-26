a148bd2e4b331417f7383bfbe6447c8d47a49311
Nowele Rechka <nowele.rechka@gmail.com>
2020-09-25 20:55:03 -0600

    #46 - BalanceParentheses setup

---
+++ Interview_Questions/BalanceParentheses.java
@@ -0,0 +1,51 @@
+/*
+https://github.com/akshitagit/JAVA/issues/46
+You are given a string of brackets i.e. '{', '}' , '(' , ')', '[' , ']' .
+You have to check whether the sequence of parenthesis is balanced or not.
+For example, "(())", "(())()" are balanced and "())(", "(()))" are not.
+Input Format
+
+A string of '(' , ')' , '{' , '}' and '[' , ']' .
+Constraints
+
+1<=|S|<=10^5
+Output Format
+
+Print "Yes" if the brackets are balanced and "No" if not balanced.
+Sample Input
+
+(())
+
+Sample Output
+
+Yes
+*/
+
+public class BalanceParentheses {
+
+    public static void balanceParentheses(String brackets) {
+
+        //Characters
+        String curlyOpen = "{";
+        String curlyClose = "}";
+        String bracketOpen = "[";
+        String bracketClose = "]";
+        String parenthesesOpen = "(";
+        String parentheseClose = ")";
+
+        String result = "";
+
+        //@TODO - determine if brackets is balanced
+
+		System.out.println("result: " + result);
+    }
+
+	public static void main(String[] args) {
+        balanceParentheses("(())");      //balanced
+        balanceParentheses("(())()");    //balanced
+        balanceParentheses("())(");      // not balanced
+        balanceParentheses("(()))");     //not balanced
+
+	}
+
+}

43cc5171ac714bdecdfdef49b05b81eb7e6badf2
Volku <horribledark@gmail.com>
2020-09-26 12:54:54 +0700

    add problem on top

--- Interview_Questions/FindLongestSubsequence.java
+++ Interview_Questions/FindLongestSubsequence.java
@@ -1,3 +1,24 @@
+/*
+Find the length of the longest subsequence in a given array A of integers such that all elements of the subsequence are sorted in strictly ascending order.
+Input Format
+
+The first line contains a single integer n.
+Next line contains n space separated numbers denoting the elements of the array.
+Constraints
+
+0 < n< 105
+0 < Ai < 105
+Output Format
+
+Print a single line containing a single integer denoting the length of the longest increasing subsequence.
+Sample Input
+
+6
+50 3 10 7 40 80
+
+Sample Output
+4
+*/
 import java.util.Scanner;
 class FindLongestSubsequence{
     public static int FLS(int[] seq){

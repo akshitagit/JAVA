d4ee58215489d032e76e2273c7e321cf036b519c
SagarGi <sagargurung1001@gmail.com>
2020-10-01 12:48:57 +0545

    Solving Anagramns

--- Patterns/PascalsTriangle.java
+++ Patterns/PascalsTriangle.java
@@ -1,31 +1,30 @@
 import java.util.Scanner;
 
-public class PascalsTriangle{     
-     public static void main (String args[]){       
-         System.out.println("Please! Enter a Number");
-         Scanner numberInput = new Scanner(System.in);
-         int number = numberInput.nextInt();
-         int spaces = number;
-         int numberNew = 1;
+public class PascalsTriangle {
+   public static void main(String args[]) {
+      System.out.println("Please! Enter a Number");
+      Scanner numberInput = new Scanner(System.in);
+      int number = numberInput.nextInt();
+      int spaces = number;
+      int numberNew = 1;
 
-         if (number <= 10){
-            for(int i=0; i < number; i++){
-               for(int s=1 ; s<=spaces; s++){
-                  System.out.print(" ");
-               }
-
-               numberNew = 1;
-               for(int j=0 ; j <= i ; j++){
-                  System.out.print(numberNew + " ");
-                  numberNew = numberNew * (i-j)/(j+1);
-               }
-               spaces--;
-               System.out.println();
+      if (number <= 10) {
+         for (int i = 0; i < number; i++) {
+            for (int s = 1; s <= spaces; s++) {
+               System.out.print(" ");
             }
+
+            numberNew = 1;
+            for (int j = 0; j <= i; j++) {
+               System.out.print(numberNew + " ");
+               numberNew = numberNew * (i - j) / (j + 1);
+            }
+            spaces--;
+            System.out.println();
          }
-         else{
-            System.out.println("Please, Enter a Valid No., Less than or Equal to 10");
-         }
-     }
+      } else {
+         System.out.println("Please, Enter a Valid No., Less than or Equal to 10");
+      }
+   }
 
 }
--- Strings/Anagrams.java
+++ Strings/Anagrams.java
@@ -1,34 +1,35 @@
-import java.util.*;
 
-class Anagrams {
-	Boolean anagrams(String a, String b){
-  
-		    char[] str1 = a.toCharArray(); 
-        char[] str2 = b.toCharArray(); 
-        int n1 = str1.length;
-        int n2 = str2.length;
+import java.util.Arrays;
 
-        if(n1 != n2){
-        	return false;
+// to check the string is anagram or not simple
+
+public class Anagrams {
+
+    public String sortString(String value) {
+        char tempArray[] = value.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public boolean anagramOutput(String s1, String s2) {
+        boolean result = false;
+
+        if (sortString(s1).equals(sortString(s2))) {
+            result = true;
         }
 
-        Arrays.sort(str1);
-        Arrays.sort(str2);
+        return result;
 
-        for (int i = 0; i < n1; i++) 
-            if (str1[i] != str2[i]) 
-                return false; 
-  
-        return true; 
-	}
-        public static void main(String args[]){
+    }
+}
 
-        	String str1 = "test"; 
-      	  	String str2 = "test"; 
-      	  	Anagrams a = new Anagrams();
-      	 	if (a.anagrams(str1, str2)) 
-      	     System.out.println("Yes"); 
-    	    else
-            System.out.println("No"); 
-    } 
+class AnagramsDemo {
+    public static void main(String args[]) {
+        String s1 = "test";
+        String s2 = "test";
+        Anagrams a = new Anagrams();
+        boolean result = a.anagramOutput(s1, s2);
+        System.out.println("Anagram = " + result);
+
+    }
 }

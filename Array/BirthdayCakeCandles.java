09f8ec1bfd15918318781d0c0829d6725b3d04f1
debasish-123 <72218056+debasish-123@users.noreply.github.com>
2020-10-10 19:46:55 +0530

    Birthday Cake Candles Problem
    1. We are given in charge of the cake for a child's birthday.
    2. Here the the cake will have one candle for each year of their total age.
    3. They will only be able to blow out the tallest of the candles.
    4. We have to count how many candles are tallest.
    5. Here we have to take the candles in an array of size n

---
+++ Array/BirthdayCakeCandles.java
@@ -0,0 +1,56 @@
+package p1;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BirthdayCakeCandles{
+	static int birthdayCakeCandles(int[] ar) {
+        
+        int m =0;
+        int count =0;
+        for(int i=0;i<ar.length;i++){
+            m=Math.max(m,ar[i]);
+        }
+        for(int i=0;i<ar.length;i++){
+            if(ar[i]==m){
+                count++;
+            }
+        }
+        return count;
+        
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
